665451b1ccb9268cf73ebbb879e3ccc95afdf092
ehxie <815438732@qq.com>
2023-07-19 14:23:38 +0800

    feat: support vue2

---
+++ scripts/rollup.config.dev.mjs
@@ -0,0 +1,46 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
+import commonjs from 'rollup-plugin-commonjs';
+import serve from 'rollup-plugin-serve';
+import { terser } from 'rollup-plugin-terser';
+import vue from 'rollup-plugin-vue';
+
+module.exports = [
+    {
+        input: 'src/main.js',
+        output: [
+            {
+                file: 'dist/index.js',
+                format: 'es'
+            }
+        ],
+        plugins: [
+            nodeResolve({
+                browser: true,
+                preferBuiltins: false
+            }),
+            commonjs(),
+            replace({
+                'process.env.NODE_ENV': JSON.stringify('production'),  // 替换`process.env.NODE_ENV`变量
+                'process': JSON.stringify({
+                    env: {
+                        NODE_ENV: 'production'  // 提供一个假的`process`全局变量
+                    }
+                }),
+                preventAssignment: true,
+            }),
+            vue({
+                // Dynamically inject css as a <style> tag
+                css: true,
+                // Explicitly convert template to render function
+                compileTemplate: true
+            }),
+            terser(),
+            serve({
+                open: true,
+                contentBase: ['public', 'dist'],
+                port: 8090
+            })
+        ]
+    }
+]
